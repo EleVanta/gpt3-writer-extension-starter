93b109d7499b6bc7a081e1a8574ac00291f98493
EleVanta <119390195+EleVanta@users.noreply.github.com>
2022-12-05 00:38:46 +0000

    content updates

--- scripts/contextMenuServiceWorker.js
+++ scripts/contextMenuServiceWorker.js
@@ -1,4 +1,50 @@
-const generateCompletionAction = async (info) => {}
+const getKey = () => {return new Promise((resolve, reject) => {
+    chrome.storage.local.get(['openai-key'], (result) => {
+      if (result['openai-key']) {
+        const decodedKey = atob(result['openai-key']);
+        resolve(decodedKey);
+      }
+    });
+  });
+};
+
+const generate = async (prompt) => {
+    const key = await getKey();
+    const url = 'https://api.openai.com/v1/completions';
+
+    const completionResponse = await fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${key}`,
+        },
+        body: JSON.stringify({
+            model: 'text-davinci-003',
+            prompt: prompt,
+            max_tokens: 1250,
+            temperature: 0.7,
+        }),
+    });
+
+    const completion = await completionResponse.json();
+  return completion.choices.pop();
+}
+
+const generateCompletionAction = async (info) => {
+    try {
+        const { selectionText } = info;
+        const basePromptPrefix = `
+        Write a description of a meal and it's recipe based type of meal, diet, and mood of the following criteria:
+        ${req.body.userInput}
+	    Also provide the length of time it will take to make the meal!
+	`;
+    const baseCompletion = await generate(`${basePromptPrefix}${selectionText}`);
+
+    console.log(baseCompletion.text)
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 chrome.contextMenus.create({
     id: 'context-run',
